4364b62a042f0419c0965c70e0ea9bc9627a59f8
tormobr <tormod.brandshoi@gmail.com>
2019-12-04 21:55:03 +0100

    day 4 part two done

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -4,18 +4,20 @@ def task1():
     l = 307237
     h = 769058
     res = 0
+    digits = []
     for i in range(l,h,1):
         if test_double(i) and test_decreasing(i):
             res += 1
+            digits.append(i)
     return res
 
 def test_double(x):
-    digit = str(x)
-    for i in range(len(str(x))-1):
-        if digit[i] == digit[i+1]:
+    length = 6
+    digit = "X" + str(x) + "X"  # add padding to borders are smooth
+    for i in range(1, length):
+        if digit[i] != digit[i-1] and digit[i] == digit[i+1] and digit[i] != digit[i+2]:
             return True
-
-    return False
+    return False 
 
 def test_decreasing(x):
     digit = str(x)
@@ -24,6 +26,5 @@ def test_decreasing(x):
             return False
 
     return True
-
 
 print(task1())
