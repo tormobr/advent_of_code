ca00324ec96ab3520cdc6bf3a65813f99a12b791
tormobr <tormod.brandshoi@gmail.com>
2019-12-04 02:33:42 +0100

    day one part two done

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,10 +1,24 @@
 import math 
 
-def solve(filename):
+def part1(filename):
     fd = open(filename, "r")
     inputs = [int(x) for x in fd]
     
-    res = sum(math.floor(i/3) - 2 for i in inputs) 
-    print(res)
+    res = sum(i//3 - 2 for i in inputs) 
+    return res
 
-solve("input.txt")
+def part2(filename):
+    fd = open(filename, "r")
+    inputs = [int(x) for x in fd]
+    res = 0
+    for mass in inputs:
+        module_fuel = mass // 3 - 2
+        while module_fuel > 0:  
+            res += module_fuel
+            module_fuel = module_fuel //3 -2
+    return res
+        
+
+print(part1("input.txt"))
+print(part2("input.txt"))
+print(hax("input.txt"))
