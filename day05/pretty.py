3d0f1d64e7b31b4358be8cf282753675922b8940
tormobr <tormod.brandshoi@gmail.com>
2019-12-05 20:04:52 +0100

    made stuff look good

--- day05/pretty.py
+++ day05/pretty.py
@@ -3,48 +3,43 @@ def part1(data):
     i = 0
     while True:
         ret = execute_code(data, i)
-        if ret == -1:
-            break
-        else:
-            i += ret
+        if ret == -1: break
+        else: i += ret
     return data[0]
 
 # executes a intcode
 def execute_code(data, i):
     opcode, param1, param2 = parse_instruction(data, i) 
-    return OPS[opcode](param1, param2, i, data)
+    return OPS[opcode](data, i, param1, param2)
 
 def parse_instruction(data, i):
-    instruction = data[i]
-    modes = [x for x in str(instruction)]
-    while len(modes) < 5:
-        modes = ["0"] + modes
-    opcode = int(modes[-2]+modes[-1])
-    param1, param2, param3 = 0, 0, 0
+    param1, param2 = 0, 0
+    instruction = "0"*(5 - len(str(data[i]))) + str(data[i])    # adds missing 0's
+    opcode = int(instruction[-2] + instruction[-1])
     if opcode not in [3,4,99]:
-        param1 = data[i+1] if  int(modes[-3]) else data[data[i+1]]
-        param2 = data[i+2] if  int(modes[-4]) else data[data[i+2]]
+        param1 = data[i+1] if  int(instruction[-3]) else data[data[i+1]]
+        param2 = data[i+2] if  int(instruction[-4]) else data[data[i+2]]
 
     return opcode, param1, param2
-    
 
-def op_add(p1, p2, i, data):
+
+def op_add(data, i, p1, p2):
     data[data[i+3]] = p1 + p2
     return 4
 
-def op_mul(p1, p2, i, data):
+def op_mul(data, i, p1, p2):
     data[data[i+3]] = p1 * p2
     return 4
 
-def op_in(p1, p2, i, data):
+def op_in(data, i, *args):
     data[data[i+1]] = 1
     return 2
 
-def op_out(p1, p2, i, data):
+def op_out(data, i, *args):
     data[0] = data[data[i+1]]
     return 2
 
-def op_halt(p1, p2, i, data):
+def op_halt(*args):
     return -1
 
 OPS = {
