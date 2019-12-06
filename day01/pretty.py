50edb87dc29d2d0709f74653cf7816a525a9847e
tormobr <tormod.brandshoi@gmail.com>
2019-12-06 18:58:22 +0100

    made recusive solution for day one part 2

--- day01/pretty.py
+++ day01/pretty.py
@@ -2,23 +2,19 @@
 def part1(data):
     return sum(calculate_fuel(mass) for mass in data) 
 
-# solution for part 2
+# solution for part 2 using recursion
 def part2(data):
-    return sum(calculate_module(mass) for mass in data)
+    return sum([calculate_module(mass) for mass in data])
+
+#  calculate the fuel for a module using recursion
+def calculate_module(mass):
+    addon = calculate_fuel(mass)
+    if addon <= 0: return 0
+    return addon + calculate_module(addon)
 
 # calculates the fuel needs for a given mass
 def calculate_fuel(mass):
     return mass // 3 - 2
-
-# advanced calculation for fuel based on mass (used in part 2)
-def calculate_module(mass):
-    total = 0
-    addon = calculate_fuel(mass)
-    while addon > 0:
-        total += addon
-        addon = calculate_fuel(addon)
-    return total
-
 
 if __name__ == "__main__":
     data = [int(x) for x in open("input.txt", "r")]
