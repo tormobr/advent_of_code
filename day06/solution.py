ccb40de0bb4649a45df61ee3d7b4e1c4fb70feaa
tormobr <tormod.brandshoi@gmail.com>
2019-12-06 09:21:02 +0100

    pretty as fuck

--- day06/solution.py
+++ day06/solution.py
@@ -1,49 +1,53 @@
 
 # solution for part 1
 def part1(data):
-    tree = {}
-    parents = {}
+    tree, parents = get_trees(data)
+    return DFS(root, tree, 0)
+
+# solution for part 2
+def part2(data):
+    tree, parents = get_trees(data)
+    return find_santa(tree, parents)
+
+# gets the direct and indirect obits using depth first search
+def DFS(node, tree, depth):
+    if leaf(node,tree): return depth
+    return sum([DFS(child, tree, depth+1) for child in tree[node]])+depth
+
+# moves ups parents of "YOU" and look for santas parent
+def find_santa(tree, parents):
+    p, ret, i = parents[me], -1, 0
+    while ret == -1:
+        ret = find_santa_recursion(p, tree, i)
+        i += 1
+        p = parents[p]
+    return ret
+
+
+# Recursively checks if santa is a child of a node
+def find_santa_recursion (node, tree, depth):
+    if node == santa: return depth -1
+    elif leaf(node, tree): return -1
+    return max([find_santa_recursion(child, tree, depth+1) for child in tree[node]])
+
+# Checks if a node is a leaf node(no kids)
+def leaf(node, tree):
+    return node not in tree.keys()
+
+
+# Generates a couple of dicts for the parent-kid relationships
+def get_trees(data):
+    parents, tree = {}, {}
     for line in data:
         A,B = line.split(")")
         if A not in tree.keys():
             tree[A] = []
         tree[A].append(B)
         parents[B] = A
-    find_santa(tree, parents)
-    return DFS(root, tree, 0)
+    return tree, parents
 
 
-def DFS(node, tree, depth):
-    if leaf(node,tree): return depth
-    return sum([DFS(child, tree, depth+1) for child in tree[node]])+depth
-
-def find_santa(tree, parents):
-    p = parents[me]
-    ans = []
-    i = 0
-    while p != "COM":
-        ans.append(rec(p, tree, i))
-        i += 1
-        p = parents[p]
-    print("answer:", min(ans))
-
-def rec (node, tree, depth):
-    if node == santa: return depth-1
-    elif leaf(node, tree): return 9999999
-    childs = []
-    for child in tree[node]:
-        childs.append(rec(child, tree, depth+1))
-    return min(childs)
-    
-        
-
-def leaf(node, tree):
-    return node not in tree.keys()
-
-# solution for part 2
-def part2(data):
-    pass
-
+# Just some juicy vars
 root = "COM"
 me = "YOU"
 santa = "SAN"
