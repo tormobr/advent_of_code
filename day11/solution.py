9f53a55efa72535433026c7431590683db018a93
tormobr <tormod.brandshoi@gmail.com>
2019-12-11 19:19:27 +0100

    pretty print better

--- day11/solution.py
+++ day11/solution.py
@@ -25,14 +25,12 @@ def move(n, dirr):
     return dirr
 
 def pretty_print(H):
-    print(H)
-    max_x =100
-    max_y = 100
-    mid = max_x // 2
+    max_x, max_y, min_x, min_y = get_dimensions(H)
     arr = [[0 for i in range(max_x)] for j in range(max_y)]
     for key, value in H.items():
-        print(key)
-        arr[mid -key[1]-1][mid-key[0]] = value
+        c_x = key[0] + abs(min_x)
+        c_y = key[1] + abs(min_y)
+        arr[c_y][c_x] = value
     res = ""
     for y in range(max_y):
         for x in range(max_x):
@@ -41,18 +39,24 @@ def pretty_print(H):
             else:
                 res += "  "
         res += "\n"
-    print(res)
+    return res
+
+def get_dimensions(H):
+    arr = [key for key in H.keys()]
+    max_x = max(arr, key=lambda x: x[0])[0]
+    max_y = max(arr, key=lambda x: x[1])[1]
+    min_x = min(arr, key=lambda x: x[0])[0]
+    min_y = min(arr, key=lambda x: x[1])[1]
+    dim_x = abs(min_x) + abs(max_x) + 1
+    dim_y = abs(min_y) + abs(max_y) + 1 
+    return dim_x, dim_y, min_x, min_y
 
 def part_n(data, inn):
-    dirr = (0,1)
-    inn = [0]
+    dirr = (0,-1)
     x,y = 0, 0
     painted = {}
     out = 0
     computer = Intcoder(data, 0)
-    outputs = []
-    total = 0
-    hax = 0
     first = True
     while out != -1:
         out = computer.eval(inn)
@@ -66,13 +70,13 @@ def part_n(data, inn):
         inn.append(painted.get((x,y),0))
 
     pretty_print(painted)
-    return len(painted)
+    return painted, len(painted)
 
 if __name__ == "__main__":
     data = [int(x) for x in open("input.txt", "r").read().split(",")]
     data = {i:x for (i,x) in enumerate(data)}
-    print(f"Part 1 answer: {part_n(data.copy(), 1)}")
-    #print(f"Part 1 answer: {part_n(data.copy(), [2])}")
+    print(f"Part 1 answer: {part_n(data.copy(), [0])[1]}")
+    print(f"Part 2 answer:\n{pretty_print(part_n(data.copy(), [1])[0])}")
 
 
 
