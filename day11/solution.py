72f33d0ddf5295fbae90eb95b348e415ee659dec
tormobr <tormod.brandshoi@gmail.com>
2019-12-12 00:42:48 +0100

    Fetching dims while iterating in robot

--- day11/solution.py
+++ day11/solution.py
@@ -1,8 +1,12 @@
+from collections import defaultdict
 import numpy as np
 from intcoder import Intcoder
 
-def pretty_print(H):
-    max_x, max_y, min_x, min_y = get_dimensions(H)
+
+# Prints the painting in a nice way
+def pretty_print(H, minmax):
+    max_x, max_y = get_dimensions(H, minmax)
+    min_x, min_y = minmax[1], minmax[3]
     print(max_x, max_y)
     arr = np.zeros((max_y, max_x), dtype=str)
     for key, value in H.items():
@@ -15,42 +19,48 @@ def pretty_print(H):
     arr = np.concatenate([arr, [["\n"]]*max_y], axis=1)
     return "".join(np.reshape(arr, ((max_x+1)*max_y)))
 
-def get_dimensions(H):
-    arr = [key for key in H.keys()]
-    max_x = max(arr, key=lambda x: x[0])[0]
-    max_y = max(arr, key=lambda x: x[1])[1]
-    min_x = min(arr, key=lambda x: x[0])[0]
-    min_y = min(arr, key=lambda x: x[1])[1]
-    dim_x = abs(min_x) + abs(max_x) + 1
-    dim_y = abs(min_y) + abs(max_y) + 1 
-    return dim_x, dim_y, min_x, min_y
+# Gets the dimensions needed for the painted pic
+def get_dimensions(H, minmax):
+    #arr = [key for key in H.keys()]
+    #max_x = max(arr, key=lambda x: x[0])[0]
+    #max_y = max(arr, key=lambda x: x[1])[1]
+    #min_x = min(arr, key=lambda x: x[0])[0]
+    #min_y = min(arr, key=lambda x: x[1])[1]
+    dim_x = abs(minmax[1]) + abs(minmax[0]) + 1
+    dim_y = abs(minmax[2]) + abs(minmax[3]) + 1 
+    return dim_x, dim_y
 
+
+def test_new_minmax(x,y, minmax):
+    if x > minmax[0]: minmax[0] = x
+    if x < minmax[1]: minmax[1] = x
+    if y > minmax[2]: minmax[2] = y
+    if y < minmax[3]: minmax[3] = y
+
+# Solves part one and part 2
 def part_n(data, inn):
     computer = Intcoder(data, 0)
     directions = [(0,-1), (1,0), (0,1), (-1,0)]
-    dirr_index = 0
-    x,y = 0, 0
-    painted = {}
-    out = 0
+    minmax = [0,0,0,0]
+    x, y, color, dir_index = 0, 0, 0, 0
+    painted = defaultdict(int)
 
-    while out != -1:
-        out = computer.eval(inn)
-        painted[(x,y)] = out
+    while color != -1:
+        color = computer.eval(inn)
+        painted[(x,y)] = color
+        test_new_minmax(x,y,minmax)
 
-        out2 = computer.eval(inn)
-        dirr_index += 1 if out2 else -1
-        x += directions[dirr_index%4][0]
-        y += directions[dirr_index%4][1]
-        inn.append(painted.get((x,y),0))
+        turn_dir = computer.eval(inn)
+        dir_index += 1 if turn_dir else -1
+        x += directions[dir_index%4][0]
+        y += directions[dir_index%4][1]
+        inn.append(painted[(x,y)])
 
-    return painted, len(painted)
+    return painted, len(painted), minmax
 
 if __name__ == "__main__":
     data = [int(x) for x in open("input.txt", "r").read().split(",")]
     data = {i:x for (i,x) in enumerate(data)}
     print(f"Part 1 answer: {part_n(data.copy(), [0])[1]}")
-    print(f"Part 2 answer:\n{pretty_print(part_n(data.copy(), [1])[0])}")
-
-
-
-
+    H,length,minmax = part_n(data.copy(), [1])
+    print(f"Part 2 answer:\n{pretty_print(H, minmax)}")
