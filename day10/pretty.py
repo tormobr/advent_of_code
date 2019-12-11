9de885536e508e4ac89bc345e2b3bfbba31e7ba3
tormobr <tormod.brandshoi@gmail.com>
2019-12-11 05:26:21 +0100

    done with prettyfying

--- day10/pretty.py
+++ day10/pretty.py
@@ -1,38 +1,34 @@
 import math
-def part1(data):
-    print([get_angles(data,x,y) for (x,y) in data])
-    best = 0
-    xy = (0,0)
-    for x,y in data:
-        angles, length = get_angles(data, x, y)
-        if length > best:
-            best = length
-            xy = (x,y)
-    return best, xy
- 
-def get_angles(data, x, y):
-    hax = {}
-    for item in data:
-        c_x, c_y = get_relative(x, y, item[0], item[1])
-        k = math.atan2(c_x, c_y)
-        if k not in hax :
-            hax[k] = []
-        hax[k].append((c_x, c_y))
+from collections import defaultdict
 
-    return hax,len(hax)
-            
+# Solution for part 1 
+def part1(data):
+    return max([(get_angles(data,x,y, length=True), x,y) for (x,y) in data])
+ 
+# Solution for part 2
 def part2(data, x, y):
     R,_ = get_angles(data, x, y)
     R = [(k, R[k]) for k in reversed(sorted(R.keys()))]
-    return (x +R[199][1][0][0])*100 + (y+R[199][1][0][1])
+    return (x +R[199][1][-1][0])*100 + (y+R[199][1][-1][1])     # Kinda hacky, but works
 
+# Creates a dict over possible angles, and the gridpoints as values
+def get_angles(data, x, y, length=False):
+    hax = defaultdict(lambda : list())
+    for x2, y2 in data:
+        r_x, r_y = get_relative(x, y, x2, y2)
+        k = math.atan2(r_x, r_y)
+        hax[k].append((r_x, r_y))
+    if length: return len(hax)
+    return hax,len(hax)
+            
+# gets the relative grid x,y from a starting point
 def get_relative(x, y, i, j):
     return i - x, j - y
 
 if __name__ == "__main__":
     lines = [line for line in open("input.txt", "r")]
     data = [(x,y) for y in range(len(lines)) for x in range(len(lines[y])) if lines[y][x] == "#"]
-    maxx, xy = part1(data)
+    maxx, x, y = part1(data)
     print(f"Part 1 answer: {maxx}")
-    print(f"Part 2 answer: {part2(data, xy[0], xy[1])}")
+    print(f"Part 2 answer: {part2(data, x, y)}")
 
