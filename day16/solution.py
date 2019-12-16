5b1fe2361899f282510f35c91552011d7b41c795
tormobr <tormod.brandshoi@gmail.com>
2019-12-16 09:06:53 +0100

    more part 1 enhancements

--- day16/solution.py
+++ day16/solution.py
@@ -1,10 +1,9 @@
 import time
 import numpy as np
 def part1(data):
-    out = data
     for i in range(100):
-        out = phase(out)
-    return out
+        data = phase(data)
+    return "".join([str(x) for x in data[:8]])
 
 def part2(data):
     print(data)
@@ -18,10 +17,6 @@ def part2(data):
         #print(out)
     return out[:9]
 
-def get_pattern(i, n):
-    basic_pattern = [0,1,0,-1] 
-    repeated = np.repeat(basic_pattern, i+1)
-    return np.roll(np.tile(repeated, n // len(repeated)+1),-1)[:n]
 
 
 def phase2(inn):
@@ -33,19 +28,28 @@ def phase2(inn):
         s -= inn[i]
     return out
     
-def last_digit(n):
-    return abs(n) % 10
 
+# executes one phase and returns output list
 def phase(inn):
     print("new phase")
     l = len(inn)
     return [last_digit(np.sum(inn * get_pattern(i, l))) for i in range(l)]
+
+# Extracts the last digit(to the right) from a integer
+def last_digit(n):
+    return abs(n) % 10
+
+# Generates the repeating pattern depening on out index and input lenght
+def get_pattern(i, n):
+    basic_pattern = [0,1,0,-1] 
+    repeated = np.repeat(basic_pattern, i+1)
+    return np.roll(np.tile(repeated, n // len(repeated)+1),-1)[:n]
 
 string = open("input.txt").read().strip()
 list_data = [int(x) for x in string]
  
 data1 = np.array(list_data)
 
-print(part1(data1.copy()))
+print(f"Part 1 answer: {part1(data1.copy())}")
 #data2 = np.array(list_data*10000)
 #print(part2(data2.copy()))
