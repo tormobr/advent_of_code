9677d0b48c207b3c17f7f4a2ee5d5aadc3bd98e4
tormobr <tormod.brandshoi@gmail.com>
2019-12-16 11:20:38 +0100

    put the plotting in own file

---
+++ day13/plotter.py
@@ -0,0 +1,22 @@
+from matplotlib import pyplot as plt
+from matplotlib import animation
+class Animater:
+    def __init__(self, arrays):
+        fig, ax = plt.subplots()
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        self.mat = ax.matshow(self.generate_data(arrays, len(arrays)-1))
+        ani = animation.FuncAnimation(fig, self.update, lambda: self.data_gen(arrays, len(arrays)), interval=10, save_count=len(arrays))
+        plt.show()
+        #ani.save("plot.mp4", )
+
+    def generate_data(self, arrays, i):
+        return arrays[i]
+
+    def update(self, data):
+        self.mat.set_data(data)
+        return self.mat 
+
+    def data_gen(self, arrays, iterations):
+        for i in range(0, iterations, 2):
+            yield self.generate_data(arrays, i)
--- day13/solution.py
+++ day13/solution.py
@@ -2,94 +2,79 @@ import time
 import os
 import numpy as np
 from intcoder import Intcoder 
-from matplotlib import pyplot as plt
-from matplotlib import animation
+from plotter import Animater
 
-def part1(data):
-    computer = Intcoder(data, 0)
-    ret = 0 
-    tile_id = 0
-    arr = np.zeros((45,23))
-    while tile_id != -1:
-        x = computer.eval([None])
-        y = computer.eval([None])
-        tile_id = computer.eval([None])
-        if tile_id == 2:
-            ret += 1
-        arr[x,y] = tile_id
-    return ret
-def draw_frame(arr):
-    ret = ""
-    for i in range(len(arr)):
-        for j in range(len(arr[0])):
-            if arr[i][j] == 0: ret += "  "
-            elif arr[i][j] == 1: ret += "X "
-            elif arr[i][j] == 2: ret += "B "
-            elif arr[i][j] == 3: ret += "_ "
-            elif arr[i][j] == 4: ret += "O "
-        ret += "\n"
-    print(ret)
+class Breakout:
     
+    def __init__(self, data):
+        self.data = data
+        self.arrays = []
+        self.data[0] = 2
+        self.computer = Intcoder(data, 0)
+        self.tile_id = 0
+        self.current_score = 0
+        self.paddle_x = 0
+        self.ball_x = 0
+        self.grid_x = 45
+        self.grid_y = 23
+        arr = np.zeros((self.grid_y,self.grid_x))
 
-def part2(data):
-    arrays = []
-    data[0] = 2
-    computer = Intcoder(data, 0)
-    ret = 0 
-    tile_id = 0
-    arr = np.zeros((23,45))
-    current_score = 0
-    paddle_x = 0
-    ball_x = 0
-    inn = None
-    counter = 0
-    scores = []
-    while tile_id != -1:
-        #if counter > 45*22:
-            #os.system("clear")
-            #draw_frame(arr)
-            #time.sleep(0.0001 / (current_score+1))
-        counter += 1
-        x = computer.eval(inn)
-        y = computer.eval(inn)
-        tile_id = computer.eval(inn)
-        if tile_id == 4:
-            ball_x = x
-        elif tile_id == 3:
-            paddle_x = x 
 
-        inn = 0 
-        if paddle_x < ball_x: inn = 1
-        elif paddle_x > ball_x: inn = -1
-        if x == -1 and y == 0:
-            current_score = tile_id
-            scores.append(current_score)
-        else:
-            if tile_id == -1:
-                break
-            arr[y,x] = tile_id
-            arrays.append(arr.copy())
-        print(current_score)
-    return max(scores), arrays
+    def part1(self, data):
+        computer = Intcoder(data, 0)
+        ret = 0 
+        tile_id = 0
+        arr = np.zeros((45,23))
+        while tile_id != -1:
+            x = computer.eval(None)
+            y = computer.eval(None)
+            tile_id = computer.eval(None)
+            if tile_id == 2:
+                ret += 1
+            arr[x,y] = tile_id
+        return ret
 
-def generate_data(arrays, i):
-    return arrays[i]
+    def part2(self, data):
+        arrays = []
+        data[0] = 2
+        computer = Intcoder(data, 0)
+        tile_id = 0
+        arr = np.zeros((23,45))
+        current_score = 0
+        paddle_x = 0
+        ball_x = 0
+        inn = None
+        while tile_id != -1:
+            x = computer.eval(inn)
+            y = computer.eval(inn)
+            tile_id = computer.eval(inn)
+            
+            self.parse_output(x, y, tile_id)
 
-def update(data):
-    mat.set_data(data)
-    return mat 
+            if tile_id == 4:
+                ball_x = x
+            elif tile_id == 3:
+                paddle_x = x 
 
-def data_gen(arrays, iterations):
-    for i in range(0, iterations, 2):
-        yield generate_data(arrays, i)
+            inn = 0 
+            if paddle_x < ball_x: inn = 1
+            elif paddle_x > ball_x: inn = -1
+
+            if x == -1 and y == 0:
+                current_score = tile_id
+            else:
+                if tile_id == -1:
+                    return current_score, arrays
+                arr[y,x] = tile_id
+                arrays.append(arr.copy())
+            print(current_score)
+
+    def parse_output(self, x,y,tile_id):
+        pass
+
 
 if __name__ == "__main__":
     data = {i:int(x) for (i,x) in  enumerate(open("input.txt", "r").read().split(","))}
-    part2_res, arrays = part2(data)
-    fig, ax = plt.subplots()
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    mat = ax.matshow(generate_data(arrays, len(arrays)-1))
-    ani = animation.FuncAnimation(fig, update, lambda: data_gen(arrays, len(arrays)), interval=10, save_count=len(arrays))
-    plt.show()
-    ani.save("plot.mp4", )
+    b = Breakout(data)
+    part2_res, arrays = b.part2(data)
+    Animater(arrays)
