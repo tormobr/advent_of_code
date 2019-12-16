a0f40d328466019f4da74771dd822463683f98c7
tormobr <tormod.brandshoi@gmail.com>
2019-12-16 12:14:41 +0100

    put the plotting in own file

--- day13/solution.py
+++ day13/solution.py
@@ -8,73 +8,51 @@ class Breakout:
     
     def __init__(self, data):
         self.data = data
-        self.arrays = []
-        self.data[0] = 2
         self.computer = Intcoder(data, 0)
-        self.tile_id = 0
-        self.current_score = 0
-        self.paddle_x = 0
-        self.ball_x = 0
-        self.grid_x = 45
-        self.grid_y = 23
-        arr = np.zeros((self.grid_y,self.grid_x))
+        self.arrays = []    # used for animation
+        self.data[0] = 2
+        self.current_score, self.paddle_x, self.ball_x = 0, 0, 0
+        self.grid_x = 45    # hardcoded from part1
+        self.grid_y = 23    # hardcoded from part1
+        self.arr = np.zeros((self.grid_y,self.grid_x))
 
-
-    def part1(self, data):
-        computer = Intcoder(data, 0)
-        ret = 0 
-        tile_id = 0
-        arr = np.zeros((45,23))
-        while tile_id != -1:
-            x = computer.eval(None)
-            y = computer.eval(None)
-            tile_id = computer.eval(None)
-            if tile_id == 2:
-                ret += 1
-            arr[x,y] = tile_id
-        return ret
-
-    def part2(self, data):
-        arrays = []
-        data[0] = 2
-        computer = Intcoder(data, 0)
-        tile_id = 0
-        arr = np.zeros((23,45))
-        current_score = 0
-        paddle_x = 0
-        ball_x = 0
+    def play(self, data):
+        self.data[0] = 2    # play for free mode
         inn = None
-        while tile_id != -1:
-            x = computer.eval(inn)
-            y = computer.eval(inn)
-            tile_id = computer.eval(inn)
-            
-            self.parse_output(x, y, tile_id)
-
-            if tile_id == 4:
-                ball_x = x
-            elif tile_id == 3:
-                paddle_x = x 
-
-            inn = 0 
-            if paddle_x < ball_x: inn = 1
-            elif paddle_x > ball_x: inn = -1
+        while True:
+            x = self.computer.eval(inn)
+            y = self.computer.eval(inn)
+            self.tile_id = self.computer.eval(inn)
+            inn = self.parse_output(x, y)
 
             if x == -1 and y == 0:
-                current_score = tile_id
-            else:
-                if tile_id == -1:
-                    return current_score, arrays
-                arr[y,x] = tile_id
-                arrays.append(arr.copy())
-            print(current_score)
+                self.current_score = self.tile_id
+                continue
+            elif self.tile_id == -1:
+                return self.current_score
 
-    def parse_output(self, x,y,tile_id):
-        pass
+            self.arr[y,x] = self.tile_id
+            self.arrays.append(self.arr.copy())
+
+    def parse_output(self, x,y):
+        # moving paddle and ball
+        if self.tile_id == 4: 
+            self.ball_x = x
+        elif self.tile_id == 3: 
+            self.paddle_x = x 
+        
+        # getting the next input for intcoder
+        if self.paddle_x < self.ball_x: 
+            return 1
+        elif self.paddle_x > self.ball_x: 
+            return -1
+        else: 
+            return 0
 
 
 if __name__ == "__main__":
     data = {i:int(x) for (i,x) in  enumerate(open("input.txt", "r").read().split(","))}
     b = Breakout(data)
-    part2_res, arrays = b.part2(data)
-    Animater(arrays)
+    score = b.play(data)
+    print(f"Part 2 answer: {score}")
+    Animater(b.arrays)
