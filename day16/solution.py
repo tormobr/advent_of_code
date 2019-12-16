0af3d70486fc1c3785e198a79b341b90cb40f338
tormobr <tormod.brandshoi@gmail.com>
2019-12-16 08:05:39 +0100

    working solutions. need to fix asap

--- day16/solution.py
+++ day16/solution.py
@@ -5,23 +5,36 @@ def part1(data):
         out = phase(out)
     return out
 
-def phase(inn):
+def part2(data):
+    print(data)
+    message_offset = int("".join([str(x) for x in data[:7]]))
+    print(message_offset)
+    print(len(data))
+    out = data
+    for i in range(100):
+        out = phase2(out)
+        print(i)
+    return out[message_offset:message_offset+9]
+
+def phase2(inn):
     print("new phase")
+    s = sum(inn)
+    out = []
+    for i in range(len(inn)):
+        out += [((s % 10)+ 10)%10]
+        s -= inn[i]
+    return out
     out = []
     
     for i in range(len(inn)):
         print("digit:", i)
         full = np.zeros((len(inn)+1))
-        #print(np.shape(full))
-        #print(np.shape(inn))
         pattern = [0,1,0,-1]
         index = 0
         filled = False
         for j in range(len(full)):
             for k in range(i+1):
                 if index < len(full):
-
-                    #print("adding here")
                     full[index] = pattern[j%4]
                     index += 1
                 else:
@@ -29,25 +42,47 @@ def phase(inn):
                     break
             if filled: break
             j += i+1
-        #print("full:", full)
         full = full[1:]
         multi = inn * full
-        #print("inn:", inn)
-        #print("full:", full)
-        #print("multi:", multi)
         total = int(np.sum(inn*full))
 
-        #for x,y in zip(inn, full):
-            #tmp = x*y
-            #total += tmp
         last = int(str(total)[-1])
         out.append(last)
-        #print(out)
-    #out = "".join([str(x) for x in out])
+    return out
+
+def phase(inn):
+    print("new phase")
+    out = []
+    
+    for i in range(len(inn)):
+        full = np.zeros((len(inn)+1))
+        pattern = [0,1,0,-1]
+        index = 0
+        filled = False
+        for j in range(len(full)):
+            for k in range(i+1):
+                if index < len(full):
+                    full[index] = pattern[j%4]
+                    index += 1
+                else:
+                    filled = True
+                    break
+            if filled: break
+            j += i+1
+        full = full[1:]
+        multi = inn * full
+        total = int(np.sum(inn*full))
+
+        last = int(str(total)[-1])
+        out.append(last)
     return out
         
 
 string = open("input.txt").read().strip()
-data = np.array([int(x) for x in string])
+list_data = [int(x) for x in string]
+ 
+data1 = np.array(list_data)
 
-print(part1(data))
+#print(part1(data1.copy()))
+data2 = np.array(list_data*10000)
+print(part2(data2.copy()))
