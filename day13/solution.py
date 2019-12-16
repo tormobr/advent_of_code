6a2945c0c99c3de6a142be352e4446da0bcc0066
tormobr <tormod.brandshoi@gmail.com>
2019-12-16 12:41:44 +0100

    day15 prettyfied

--- day13/solution.py
+++ day13/solution.py
@@ -1,11 +1,8 @@
-import time
-import os
 import numpy as np
 from intcoder import Intcoder 
 from plotter import Animater
 
 class Breakout:
-    
     def __init__(self, data):
         self.data = data
         self.computer = Intcoder(data, 0)
@@ -15,30 +12,32 @@ class Breakout:
         self.grid_x = 45    # hardcoded from part1
         self.grid_y = 23    # hardcoded from part1
         self.arr = np.zeros((self.grid_y,self.grid_x))
-
-    def play(self, data):
+    
+    # AI plays a good game of breakout
+    def play(self):
         self.data[0] = 2    # play for free mode
         inn = None
         while True:
             x = self.computer.eval(inn)
             y = self.computer.eval(inn)
-            self.tile_id = self.computer.eval(inn)
-            inn = self.parse_output(x, y)
+            tile_id = self.computer.eval(inn)
+            inn = self.parse_output(x, y, tile_id)
 
             if x == -1 and y == 0:
-                self.current_score = self.tile_id
+                self.current_score = tile_id
                 continue
-            elif self.tile_id == -1:
+            elif tile_id == -1:
                 return self.current_score
 
-            self.arr[y,x] = self.tile_id
+            self.arr[y,x] = tile_id
             self.arrays.append(self.arr.copy())
-
-    def parse_output(self, x,y):
+    
+    # Parses the output from intcode computer and acts accordingly
+    def parse_output(self, x, y, tile_id):
         # moving paddle and ball
-        if self.tile_id == 4: 
+        if tile_id == 4: 
             self.ball_x = x
-        elif self.tile_id == 3: 
+        elif tile_id == 3: 
             self.paddle_x = x 
         
         # getting the next input for intcoder
@@ -53,6 +52,6 @@ class Breakout:
 if __name__ == "__main__":
     data = {i:int(x) for (i,x) in  enumerate(open("input.txt", "r").read().split(","))}
     b = Breakout(data)
-    score = b.play(data)
+    score = b.play()
     print(f"Part 2 answer: {score}")
     Animater(b.arrays)
