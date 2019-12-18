3b92e7ed044a4cb998d667b51c8948e33773d894
tormobr <tormod.brandshoi@gmail.com>
2019-12-18 12:39:30 +0100

    part 2 done ggsss2

--- day15/solution.py
+++ day15/solution.py
@@ -5,7 +5,7 @@ from intcoder import Intcoder
 import os
 import sys
 from copy import deepcopy
-sys.setrecursionlimit(10**6) 
+
 def draw_frame(arr):
     #os.system("clear")
     ret = ""
@@ -22,68 +22,53 @@ def draw_frame(arr):
 
 def part1(data):
     computer = Intcoder(data, 0)
-    directions = {1:(0,-1), 2:(0,1), 3:(-1,0), 4:(1,0)}
-    out = 0
-    found = False
-    x, y = 0, 0
-    max_x = 50
-    max_y = 50
-    base_x = (max_x//2) +1
-    base_y = 25
+    max_x = 42
+    max_y = 42
+    base_x = (max_x//2)
+    base_y = (max_y//2)
     arr = [[0]*max_x for i in range(max_y)]
-    arr[base_y+y][base_x+x] = 2
+    #arr[base_y+y][base_x+x] = 2
     count = 0
     visited = defaultdict(int)
     haxors = [2,1,3,4]
     results = []
-    """
-    while out != -1:
-        hax = random.randrange(1,5)
-        n = 0
-        print("is this loopping")
-        while (x+directions[hax][0], y+directions[hax][1]) in visited.keys() and n <= 5:
-            print("foreever?")
-            hax = random.randrange(1,5)
-            n += 1
-        #hax = int(input())
-        #time.sleep(4)
-        if True:
-            visited[(x+directions[hax][0],y+directions[hax][1])] += 1 
-            dirr = hax
-            print(dirr)
-            inn = dirr
-            out = computer.eval(inn)
-            if out == 0:
-                #print("hit wall")
-                t_x = base_x+x+directions[dirr][0]
-                t_y = base_y+y+directions[dirr][1]
-                arr[t_y][t_x] = 3
-            elif out == 1:
-                #print("allews gut ")
-                count += 1
-                arr[base_y+y][base_x+x] = 1
-                print("visited: ", x,y)
-                x += directions[dirr][0]
-                y += directions[dirr][1]
-                arr[base_y+y][base_x+x] = 2
-            elif out == 2:
-                print("doons")
-                found = True
-            print("out", out)
-        os.system("clear")
-        draw_frame(arr)
-        """
-    rec(computer, arr,  base_x, base_y, 1, results, 0)
-
-    return results
+    res = rec(computer, arr,  base_x, base_y, 1, results, 0)
+    #print(res)
+    print(arr)
+    print(get_depth(arr, 9, 3, 0))
+    
+    #return max(results)
 
 directions = {1:(0,-1), 2:(0,1), 3:(-1,0), 4:(1,0)}
 backtracking = {1: 2, 2: 1, 3:4, 4:3}
+
+visited2 = set()
+def get_depth(arr, x, y, depth):
+    print("current_val:", arr[y][x])
+    arr[y][x] = 2
+    draw_frame(arr) 
+    time.sleep(0.1)
+    visited.add((x,y))
+    print(depth, x,y)
+    depths = [0,0,0,0]
+    if arr[y][x] == 1:
+        return 0
+    for i in range(1,5):
+        print("direction i: ", i)
+        new_x = x+directions[i][0]
+        new_y = y+directions[i][1]
+        print("potential next val: ", arr[new_y][new_x])
+        if arr[new_y][new_x] == 1:
+            print("recusing")
+            depths[i-1] = get_depth(arr, new_x, new_y, depth+1)
+    print("depths: ",depths)
+    return max(depths) +1
+
 visited = set()
 def rec(computer,arr, x, y, steps, results, inputt):
     steppers = []
     print(steps)
-    time.sleep(.01)
+    time.sleep(.001)
     arr[y][x] = 2
     visited.add((x,y))
     for i in range(1,5):
@@ -111,16 +96,15 @@ def rec(computer,arr, x, y, steps, results, inputt):
             arr[y][x] = 2
             #print("backtrach")
         elif out == 2:
+            print(new_x, new_y)
             arr[new_y][new_x] = 4
-            results.append(steps)
             print("doons", results)
             computer.eval(backtracking[i])
-            #time.sleep(2)
             draw_frame(arr)
-            return
+    
+    results.append(steps)
     #print("bacxktrascking")
     arr[y][x] = 1
-    return 100
 
 if __name__ == "__main__":
     data = [int(x) for x in open("input.txt", "r").read().split(",")]
