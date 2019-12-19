6f5cd01f9d53dabca6b5fc009bc385f567922a5e
tormobr <tormod.brandshoi@gmail.com>
2019-12-20 00:06:58 +0100

    part 2 done

--- day19/solution.py
+++ day19/solution.py
@@ -1,64 +1,61 @@
+import time
 import numpy as np
 from intcoder import Intcoder
 
-def part1(data):
-    arr = np.zeros((1000,1000))
-
+def part1(data, val):
+    dim = val*100
+    arr = np.zeros((dim,dim))
     start_x = 0
-    for i in range(1000):
-        print(i, np.count_nonzero(arr[i-1] == 1))
+    last_xs = [0]*val
+    i = val
+    while True:
         found = False
-        for j in range(start_x, 1000):
+        print("start_x : ", start_x)
+        for j in range(start_x, dim):
             computer = Intcoder(data.copy(), 0)
             out = computer.eval([j,i])    
+            arr[i][j] = out
             if out and not found:
                 found = True
                 start_x = j
             if found and not out:
+                print("strudel")
+                last_xs.append(j-1)
+                print("breaking at: ", j)
                 break
 
+            done, res_i,res_j  = test_array(arr, i,last_xs[i-val], val=val)
+            if done:
+                print(done, res_i, res_j)
+                return (10000*res_j)+res_i
+        print("haxor" , i, np.sum(arr[i]))
+        i += 1
 
-            arr[i][j] = out
-
-    print(print_beam(arr))
     return np.count_nonzero(arr == 1)
 
-def part2(data):
-    arr = None
-    for dim in range(34,37):
-        print("new dim :" , dim)
-        start_x = dim-1
-        end_x = 0
-        arr = np.zeros((dim+1,dim+1))
-
-        for i in range(dim):
-            for j in range(dim):
-                computer = Intcoder(data.copy(), 0)
-                out = computer.eval([j,i])    
-                arr[i][j] = out
-        print(test_array(arr))        
-    return np.count_nonzero(arr == 1)
-
-def test_array(a):
-    for i in range(len(a)):
-        if np.count_nonzero(a[i] == 1) == 10:
-            hax = np.count_nonzero(a[i+10] == 1)
-            if hax == 10:
-                return True, i
-    return False, 0
+def test_array(a, current_index, last_x, val=99):
+    i = current_index-val
+    if a[i][last_x-val] == 1 and a[i+val][last_x-val] == 1:
+        return True, i, last_x-val
+    return False, 0, 0
 
 def print_beam(arr):
-    arr = np.where(arr == 1, "#", "  ")
-    res = " "
+    res = ""
     for y in arr:
         for x in y:
-            res+=x
+            if x == 1:
+                res += "X"
+            else:
+                res += " "
         res += "\n"
     print(res)
 
 if __name__ == "__main__":
-
     data = {i: int(x) for (i,x) in enumerate(open("input.txt", "r").read().split(","))}
     
-    print(part1(data))
+    print(part1(data,99))
     #print(part2(data))
+
+    
+
+
