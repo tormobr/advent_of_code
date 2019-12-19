d61946f794cb53f225c171b174df50977e8d9506
tormobr <tormod.brandshoi@gmail.com>
2019-12-20 00:49:10 +0100

    some sort of animation added

--- day19/solution.py
+++ day19/solution.py
@@ -1,41 +1,49 @@
 import time
 import numpy as np
 from intcoder import Intcoder
+from plotter import Animater
+from copy import deepcopy
 
 def part1(data, val):
-    dim = val*100
+    dim = val*15
     arr = np.zeros((dim,dim))
+    arrays = []
     start_x = 0
     last_xs = [0]*val
     i = val
-    while True:
+    ship_loc = False
+    while i < dim:
+        arrays.append(deepcopy(arr))
         found = False
-        print("start_x : ", start_x)
         for j in range(start_x, dim):
             computer = Intcoder(data.copy(), 0)
             out = computer.eval([j,i])    
-            arr[i][j] = out
+            arr[i,j] = out
             if out and not found:
                 found = True
                 start_x = j
             if found and not out:
-                print("strudel")
                 last_xs.append(j-1)
-                print("breaking at: ", j)
                 break
 
+        if not ship_loc:
             done, res_i,res_j  = test_array(arr, i,last_xs[i-val], val=val)
-            if done:
-                print(done, res_i, res_j)
-                return (10000*res_j)+res_i
+        if done:
+            ship_loc = True
+            #print(done, res_i, res_j)
+            #print(arr)
+            #return (10000*res_j)+res_i
         print("haxor" , i, np.sum(arr[i]))
         i += 1
-
+    for _ in range(200):
+        arrays.append(deepcopy(arr))
+    a = Animater(arrays)
     return np.count_nonzero(arr == 1)
 
 def test_array(a, current_index, last_x, val=99):
     i = current_index-val
     if a[i][last_x-val] == 1 and a[i+val][last_x-val] == 1:
+        a[i:i+val+1, last_x-val: last_x+1] = 2
         return True, i, last_x-val
     return False, 0, 0
 
@@ -53,7 +61,7 @@ def print_beam(arr):
 if __name__ == "__main__":
     data = {i: int(x) for (i,x) in enumerate(open("input.txt", "r").read().split(","))}
     
-    print(part1(data,99))
+    print(part1(data,9))
     #print(part2(data))
 
     
