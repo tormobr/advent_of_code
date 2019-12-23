ea4af3062797ccee4abaa5b89a40419d051bee74
tormobr <tormod.brandshoi@gmail.com>
2019-12-23 12:01:31 +0100

    commit before numba tryuout

--- day18/solution.py
+++ day18/solution.py
@@ -1,6 +1,4 @@
-from numba import jit
 from copy import deepcopy
-from plotter import Animater
 from collections import defaultdict, namedtuple, deque
 import time
 
@@ -21,41 +19,17 @@ class Maze_solver:
     def part1(self):
         return min(self.BFS())
 
-    def convert_array(self, a):
-        for i in range(len(a)):
-            for j in range(len(a[i])):
-                if a[i][j] == "#": a[i][j] = 1
-                elif a[i][j] == ".": a[i][j] = 3
-                elif a[i][j] == "@": a[i][j] = 2
-                elif a[i][j] in self.keys: a[i][j] = 4
-                elif a[i][j] in self.doors: a[i][j] = 5
-                else:
-                    a[i][j] = 6
-        return a
-
-    def track(self, path, arr):
-        for x,y in path:
-            if arr[y][x] not in [4, 5]:
-                prev = arr[y][x]
-                arr[y][x] = 7
-                self.arrays.append(deepcopy(arr))
-                arr[y][x] = prev
-            else:
-                arr[y][x] = 3
-
     def BFS(self):
-        draw_array = self.convert_array(deepcopy(self.data))
         visited = set()
         State = namedtuple("State","x y keys")
         x, y = self.start
-        path = []
         current = State(x, y, [])
         queue = deque()
-        queue.append((current, 0, path))
+        queue.append((current, 0))
         results = []
 
         while queue:
-            current,steps, path = queue.popleft()
+            current,steps = queue.popleft()
             val = self.data[current.y][current.x]
             key = (current.x, current.y, tuple(sorted(current.keys)))
             new_keys = set(current.keys)
@@ -63,7 +37,6 @@ class Maze_solver:
             if val == "#" or key in visited:
                 continue
             visited.add(key)
-            path.append((current.x, current.y))
 
             if val in self.doors and val.lower() not in current.keys:
                 continue
@@ -73,7 +46,6 @@ class Maze_solver:
                 print(new_keys)
                 if new_keys == self.keys:
                     results.append(steps)
-                    self.track(path, draw_array)
                     return results
 
             #self.data[current.y][current.x] = "."
@@ -81,7 +53,7 @@ class Maze_solver:
                 new_x = current.x + d[0]
                 new_y = current.y + d[1]
                 new_state = State(new_x, new_y, new_keys)
-                queue.append((new_state, steps+1, path.copy()))
+                queue.append((new_state, steps+1))
             
 
         print(results)
@@ -113,5 +85,4 @@ if __name__ == "__main__":
     s = time.time()
     print(m.part1())
     e = time.time()
-    Animater(m.arrays)
     print(f"runtime: {e-s}")
