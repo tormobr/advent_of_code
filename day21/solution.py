42e4314d779961fbed728f0dfe5dc0b8864a0d8b
tormobr <tormod.brandshoi@gmail.com>
2019-12-26 00:22:32 +0100

    pretty code part 1 and 2

--- day21/solution.py
+++ day21/solution.py
@@ -1,64 +1,29 @@
 from intcoder import Intcoder
-from copy import deepcopy
-import numpy as np
 
-
-
-def part2(data):
+# Solution for part 1 and 2
+def part1(data, part=1):
     computer = Intcoder(data,0)
-    inputs = []
-    inputs.append("NOT A J\n")
-    inputs.append("NOT B T\n")
-    inputs.append("OR T J\n")
-    inputs.append("NOT C T\n")
-    inputs.append("OR T J\n")
-    inputs.append("AND D J\n")
-    inputs.append("NOT I T\n")
-    inputs.append("NOT T T\n")
-    inputs.append("OR F T\n")
-    inputs.append("AND E T\n")
-    inputs.append("OR H T\n")
-    inputs.append("AND T J\n")
-    inputs.append("RUN\n")
-    ascii_in = [ord(c) for s in inputs for c in s]
+    if part == 1:
+        ascii_in = [ord(c) for c in program1()]
+    else: 
+        ascii_in = [ord(c) for c in program2()]
     out = 1
-    res = ""
     while out != -1:
         out = computer.eval(ascii_in)
-        if out <= 0x110000 and out != -1:
-            res += chr(out)
-        else:
+        if out > 255:
             return out
-        print(res)
-    return data
-    
+    return -1 
 
-def part1(data):
-    computer = Intcoder(data,0)
-    inputs = []
-    inputs.append("NOT A J\n")
-    inputs.append("NOT B T\n")
-    inputs.append("OR T J\n")
-    inputs.append("NOT C T\n")
-    inputs.append("OR T J\n")
-    inputs.append("NOT C T\n")
-    inputs.append("AND D J\n")
-    inputs.append("WALK\n")
-    ascii_in = [ord(c) for s in inputs for c in s]
-    out = 1
-    res = ""
-    while out != -1:
-        out = computer.eval(ascii_in)
-        if out <= 0x110000 and out != -1:
-            res += chr(out)
-        else:
-            return out
-        print(res)
-    return data
-    
+# fetches spring script program 1
+def program1():
+    return  "".join([line for line in open("prog1.txt")])
 
+# fetches spring script program 2
+def program2():
+    return  "".join([line for line in open("prog2.txt")])
 
-data = {i:int(x) for (i,x) in enumerate(open("input.txt", "r").read().split(","))}
+if __name__ == "__main__":
+    data = {i:int(x) for (i,x) in enumerate(open("input.txt", "r").read().split(","))}
 
-print(f"Part 1 answer: {part1(data.copy())}")
-print(f"Part 2 answer: {part2(data.copy())}")
+    print(f"Part 1 answer: {part1(data.copy())}")
+    print(f"Part 2 answer: {part1(data.copy(), part=2)}")
