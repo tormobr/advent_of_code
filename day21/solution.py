b4b33e9c07067c7e0a698b1efe15aa74ded1284c
tormobr <tormod.brandshoi@gmail.com>
2019-12-26 00:00:10 +0100

    part 1 done

---
+++ day21/solution.py
@@ -0,0 +1,33 @@
+from intcoder import Intcoder
+from copy import deepcopy
+import numpy as np
+
+
+def part1(data):
+    computer = Intcoder(data,0)
+    inputs = []
+    inputs.append("NOT A J\n")
+    inputs.append("NOT B T\n")
+    inputs.append("OR T J\n")
+    inputs.append("NOT C T\n")
+    inputs.append("OR T J\n")
+    inputs.append("NOT C T\n")
+    inputs.append("AND D J\n")
+    inputs.append("RUN\n")
+    ascii_in = [ord(c) for s in inputs for c in s]
+    out = 1
+    res = ""
+    while out != -1:
+        out = computer.eval(ascii_in)
+        if out <= 0x110000 and out != -1:
+            res += chr(out)
+        else:
+            return out
+        print(res)
+    return data
+    
+
+
+data = {i:int(x) for (i,x) in enumerate(open("input.txt", "r").read().split(","))}
+
+print(part1(data))
