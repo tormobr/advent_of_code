683e499f1d865f56b45b2c6a5a1e9b18b9a1b356
tormobr <tormod.brandshoi@gmail.com>
2019-12-26 00:07:07 +0100

    part 2 done

--- day21/solution.py
+++ day21/solution.py
@@ -3,6 +3,36 @@ from copy import deepcopy
 import numpy as np
 
 
+
+def part2(data):
+    computer = Intcoder(data,0)
+    inputs = []
+    inputs.append("NOT A J\n")
+    inputs.append("NOT B T\n")
+    inputs.append("OR T J\n")
+    inputs.append("NOT C T\n")
+    inputs.append("OR T J\n")
+    inputs.append("AND D J\n")
+    inputs.append("NOT I T\n")
+    inputs.append("NOT T T\n")
+    inputs.append("OR F T\n")
+    inputs.append("AND E T\n")
+    inputs.append("OR H T\n")
+    inputs.append("AND T J\n")
+    inputs.append("RUN\n")
+    ascii_in = [ord(c) for s in inputs for c in s]
+    out = 1
+    res = ""
+    while out != -1:
+        out = computer.eval(ascii_in)
+        if out <= 0x110000 and out != -1:
+            res += chr(out)
+        else:
+            return out
+        print(res)
+    return data
+    
+
 def part1(data):
     computer = Intcoder(data,0)
     inputs = []
@@ -13,7 +43,7 @@ def part1(data):
     inputs.append("OR T J\n")
     inputs.append("NOT C T\n")
     inputs.append("AND D J\n")
-    inputs.append("RUN\n")
+    inputs.append("WALK\n")
     ascii_in = [ord(c) for s in inputs for c in s]
     out = 1
     res = ""
@@ -30,4 +60,5 @@ def part1(data):
 
 data = {i:int(x) for (i,x) in enumerate(open("input.txt", "r").read().split(","))}
 
-print(part1(data))
+print(f"Part 1 answer: {part1(data.copy())}")
+print(f"Part 2 answer: {part2(data.copy())}")
