fa6c3535554a43720d4b0fd2ce69dc3e250ee9e3
tormobr <tormod.brandshoi@gmail.com>
2019-12-26 20:26:53 +0100

    part 2 fast and dirty.. needs refactor, goodnight

--- day23/solution.py
+++ day23/solution.py
@@ -8,12 +8,15 @@ class Network:
         self.n = n
         self.data = data
         self.inputs = [deque([i]) for i in range(self.n)]
+        self.NAT = (0,0)
+        self.SEEN = []
 
     def part1(self):
         C = []
         for i in range(self.n):
             C.append(Intcoder(self.data.copy(), i))
 
+        idle = 0
         while True:
             for i,c in enumerate(C):
                 a = c.eval(self.get_input)
@@ -22,16 +25,24 @@ class Network:
                 print(a, x, y)
                 if a == None:
                     continue
+                idle = 0
 
                 if a == 255:
-                    print(f"result: {y}")
-                    time.sleep(10)
+                    self.NAT = (x, y)
+                    continue
                 elif a not in range(50):
                     time.sleep(.4)
                     continue
                 self.inputs[a].append(x)
                 self.inputs[a].append(y)
-
+            if idle > 2:
+                self.inputs[0].append(self.NAT[0])
+                self.inputs[0].append(self.NAT[1])
+                if len(self.SEEN) > 0 and self.NAT[1] == self.SEEN[-1]:
+                    print(f"result: {self.NAT[1]}")
+                    time.sleep(10)
+                self.SEEN.append(self.NAT[1])
+            idle += 1
     def get_input(self, i):
         if len(self.inputs[i]) == 0:
             #print(f"input for {i} = -1")
