c817118776a4d2e64d885a55e5be8eec014c91b8
tormobr <tormod.brandshoi@gmail.com>
2020-01-03 15:58:39 +0100

    part 2 still blank

--- day22/solution.py
+++ day22/solution.py
@@ -1,8 +1,9 @@
+import time
 
-def part1(data, n):
-    cards = [i for i in range(n)]
+def part1(data, deck_size):
+    cards = [i for i in range(deck_size)]
     for line in data:
-        print(line)
+        #print(line)
         n = 0
         if "deal into new" not in line:
             n = int(line.split()[-1])
@@ -13,15 +14,48 @@ def part1(data, n):
             cards = cards[n:] + cards[:n]
 
         if "deal with inc" in line:
-            new_cards = [0 for i in range(n)] 
+            new_cards = [0 for i in range(deck_size)] 
             index = 0
-            for i in range(n):
-                new_cards[index%n] = cards[i]
+            for i in range(deck_size):
+                new_cards[index%deck_size] = cards[i]
                 #print("new_cards:", new_cards)
                 index += n 
             cards = new_cards.copy()
-    print(cards.index(2019))
+    return cards.index(2019)
+
+
+def deal_into_new(index, deck_size):
+    return (-index - 1) % deck_size
+
+def cut(index, deck_size, n):
+    return (index - n) % deck_size
+
+def deal_with_inc(index, deck_size, n):
+    return (index * n) % deck_size
+
+def part2(data, deck_size, target):
+    for line in data:
+        n = 0
+        if "deal into new" in line:
+            target = deal_into_new(target, deck_size)
+        else:
+            n = int(line.strip().split()[-1])
+
+        if "cut" in line:
+            target = cut(target, deck_size, n)
+
+        elif "deal with inc" in line:
+            target = deal_with_inc(target, deck_size, n)
+    return target
 
 if __name__ == "__main__":
     data = [line.strip() for line in open("input.txt")]
-    part1(data)
+    s = time.time()
+    print("Part 1 answer:", part1(data, 10007))
+    e = time.time()
+    print("Runtime: ", e-s)
+    n = 10
+    s = time.time()
+    print("Part 2 answer: ",part2(data, 119315717514047, 2019))
+    e = time.time()
+    print("Runtime: ", e-s)
